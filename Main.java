958080a3c61033ac47c1f94ce9bf0944f1496622
Sunkireddy.Javahar Reddy <89154426+javaharreddy@users.noreply.github.com>
2023-09-05 15:17:27 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
   public static void main(String args[]){
-    ystem.out.println("hello wold");
+    System.out.println("hello wold");
     System.out.println("eb hook is workin");
   }  
 }
