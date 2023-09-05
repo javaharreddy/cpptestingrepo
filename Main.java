2a4b20d54e9c25f8349b7e4f11dbef98e10de88b
Sunkireddy.Javahar Reddy <89154426+javaharreddy@users.noreply.github.com>
2023-09-05 15:12:32 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
   public static void main(String args[]){
     System.out.println("hello world");
-    System.out.println("web hook is workin");
+    System.out.println("eb hook is workin");
   }  
 }
